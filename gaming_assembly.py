52147da3e60066837fb6966bd4d17f8fb079d6f0
mikaelsrozee <mikaelsrozee@gmail.com>
2019-09-22 17:51:11 +0100

    code cleanup 3

--- gaming_assembly.py
+++ gaming_assembly.py
@@ -2,7 +2,6 @@
 # uses pyparticles to create displayed game
 
 import itertools
-import math
 import pickle
 import random
 import time
@@ -33,30 +32,97 @@ BLACK = (0, 0, 0)
 
 # Train to create save file of best racers, Race to race them on a starting grid
 Train = True
-Race = True
+Race = False
 
-if Train:
+
+def update_screen(env, screen, particle_list, track_image, track_rect):
+    font = pygame.font.Font(None, 32)
+    header = font.render('LEADERBOARD', True, RED, WHITE)
+    header_rect = header.get_rect()
+    header_rect.center = (800 + 200, 40)
+
+    env.update()
+    screen.fill(env.colour)
+    screen.blit(track_image, track_rect)
+
+    # draw cars
+    for car in env.particles:
+        pygame.draw.circle(screen, car.colour, (int(car.x), int(car.y)), car.size, car.thickness)
+
+        sorted_list = sorted(particle_list, key=lambda particle: particle.score)[::-1]
+
+        # draw leaderboard
+        screen.blit(header, header_rect)
+
+        for i in range(0, 10):
+            car = sorted_list[i]
+
+            leader_x = 800 + 75
+            leader_y = 80 + i * 35
+
+            leader_id = font.render(str(i + 1), True, BLACK, WHITE)
+            number_rect = leader_id.get_rect()
+            number_rect.center = (leader_x - 10, leader_y)
+            screen.blit(leader_id, number_rect)
+
+            pygame.draw.circle(screen, car.colour, (leader_x + 20, leader_y), car.size, car.thickness)
+
+            leader_name = font.render(str(car.name), True, BLACK, WHITE)
+            name_rect = leader_name.get_rect()
+            name_rect.midleft = (leader_x + 45, leader_y)
+            screen.blit(leader_name, name_rect)
+
+            leader_score = font.render(str(round(car.score, 3)), True, BLACK, WHITE)
+            score_rect = leader_score.get_rect()
+            score_rect.midleft = (leader_x + 100, leader_y)
+            screen.blit(leader_score, score_rect)
+
+            if car.w:
+                display_w = font.render('W', True, BLACK, WHITE)
+            else:
+                display_w = font.render('W', True, GREY, WHITE)
+            display_w_rect = display_w.get_rect()
+            display_w_rect.center = (leader_x + 190, leader_y)
+            screen.blit(display_w, display_w_rect)
+
+            if car.a:
+                display_a = font.render('A', True, BLACK, WHITE)
+            else:
+                display_a = font.render('A', True, GREY, WHITE)
+            display_a_rect = display_a.get_rect()
+            display_a_rect.center = (leader_x + 212, leader_y)
+            screen.blit(display_a, display_a_rect)
+
+            if car.s:
+                display_s = font.render('S', True, BLACK, WHITE)
+            else:
+                display_s = font.render('S', True, GREY, WHITE)
+            display_s_rect = display_s.get_rect()
+            display_s_rect.center = (leader_x + 230, leader_y)
+            screen.blit(display_s, display_s_rect)
+
+            if car.d:
+                display_d = font.render('D', True, BLACK, WHITE)
+            else:
+                display_d = font.render('D', True, GREY, WHITE)
+            display_d_rect = display_d.get_rect()
+            display_d_rect.center = (leader_x + 250, leader_y)
+            screen.blit(display_d, display_d_rect)
+    pygame.display.flip()
+
+
+def train():
     # initiate display and display options
     screen = pygame.display.set_mode((width, height))
-    lines = False
-    display_checkpoints = True
 
     # initialise environment
     env = pyparticles.Environment((width, height), image=track, checkpoints=checkpoints, colliding=False)
 
     # add initial particles
     for i in range(generation_size):
-        fov = random.uniform(0, 90)
         env.addParticles(1, x=checkpoints[0][0], y=checkpoints[0][1], speed=0, size=5)
 
-    # display text
     pygame.init()
-    pygame.display.set_caption('Generation 1')
-    basicfont = pygame.font.Font(None, 32)
-    text_string = 'LEADERBOARD'
-    header = basicfont.render(text_string, True, RED, WHITE)
-    headerRect = header.get_rect()
-    headerRect.center = (800 + 200, 40)
 
     n = 0
     while n < n_generations:
@@ -79,110 +145,24 @@ if Train:
         start_time = time.time()
         current_time = time.time()
 
-        while current_time - start_time < duration and running == True:
+        while current_time - start_time < duration and running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-            # draw background
-            env.update()
-            screen.fill(env.colour)
-            screen.blit(track_image, track_rect)
+            for pos in checkpoints:
+                pygame.draw.circle(screen, RED, pos, 5, 5)
 
-            # draw cars and their lines, if requested
-            for p in env.particles:
-                angle = math.pi - p.angle
-                fov = p.fov * math.pi / 180
-                if lines:
-                    pygame.draw.line(screen, p.colour, (int(p.x), int(p.y)), (
-                        int(p.x) + p.distance_front * math.sin(angle), int(p.y) + p.distance_front * math.cos(angle)))
-                    pygame.draw.line(screen, p.colour, (int(p.x), int(p.y)), (
-                        int(p.x) + p.distance_left * math.sin(angle + fov),
-                        int(p.y) + p.distance_left * math.cos(angle + fov)))
-                    pygame.draw.line(screen, p.colour, (int(p.x), int(p.y)), (
-                        int(p.x) + p.distance_right * math.sin(angle - fov),
-                        int(p.y) + p.distance_right * math.cos(angle - fov)))
+            update_screen(env, screen, particle_list, track_image, track_rect)
 
-                pygame.draw.circle(screen, p.colour, (int(p.x), int(p.y)), p.size, p.thickness)
-
-            sorted_list = sorted(particle_list, key=lambda particle: particle.score)[::-1]
-
-            # draw leaderboard: particle number, colour, score, controls
-            screen.blit(header, headerRect)
-
-            for i in range(0, 10):
-
-                p = sorted_list[i]
-
-                leader_x = 800 + 75
-                leader_y = 80 + i * 35
-
-                numberplate = basicfont.render(str(i + 1), True, BLACK, WHITE)
-                numberRect = numberplate.get_rect()
-                numberRect.center = (leader_x - 10, leader_y)
-                screen.blit(numberplate, numberRect)
-
-                pygame.draw.circle(screen, p.colour, (leader_x + 20, leader_y), p.size, p.thickness)
-
-                nameplate = basicfont.render(str(p.name), True, BLACK, WHITE)
-                nameRect = nameplate.get_rect()
-                nameRect.midleft = (leader_x + 45, leader_y)
-                screen.blit(nameplate, nameRect)
-
-                scoreplate = basicfont.render(str(round(p.score, 3)), True, BLACK, WHITE)
-                scoreRect = scoreplate.get_rect()
-                scoreRect.midleft = (leader_x + 100, leader_y)
-                screen.blit(scoreplate, scoreRect)
-
-                if p.w:
-                    wplate = basicfont.render('W', True, BLACK, WHITE)
-                else:
-                    wplate = basicfont.render('W', True, GREY, WHITE)
-                wRect = wplate.get_rect()
-                wRect.center = (leader_x + 190, leader_y)
-                screen.blit(wplate, wRect)
-
-                if p.a:
-                    aplate = basicfont.render('A', True, BLACK, WHITE)
-                else:
-                    aplate = basicfont.render('A', True, GREY, WHITE)
-                aRect = aplate.get_rect()
-                aRect.center = (leader_x + 212, leader_y)
-                screen.blit(aplate, aRect)
-
-                if p.s:
-                    splate = basicfont.render('S', True, BLACK, WHITE)
-                else:
-                    splate = basicfont.render('S', True, GREY, WHITE)
-                sRect = splate.get_rect()
-                sRect.center = (leader_x + 230, leader_y)
-                screen.blit(splate, sRect)
-
-                if p.d:
-                    dplate = basicfont.render('D', True, BLACK, WHITE)
-                else:
-                    dplate = basicfont.render('D', True, GREY, WHITE)
-                dRect = dplate.get_rect()
-                dRect.center = (leader_x + 250, leader_y)
-                screen.blit(dplate, dRect)
-
-            # draw checkpoints
-            if display_checkpoints:
-                for pos in checkpoints:
-                    pygame.draw.circle(screen, RED, pos, 5, 5)
-
-            pygame.display.flip()
             current_time = time.time()
             env.time_elapsed = int(round((current_time - start_time) * 1000))
 
         # breed new generation
-
         sorted_list = sorted(particle_list, key=lambda particle: particle.score)[::-1]
-
         env = pyparticles.Environment((width, height), image=track, checkpoints=checkpoints, colliding=False)
 
         for i in range(n_to_keep - 1):
-
             parent_pairs = list(itertools.combinations(range(i + 1), 2))
 
             for pair in parent_pairs:
@@ -207,16 +187,15 @@ if Train:
 
         n += 1
 
-if Race:
+
+def race():
     # load in drivers
-    with open('final_drivers', 'rb') as input:
-        driver_list = pickle.load(input)
+    with open('final_drivers', 'rb') as driver_file:
+        driver_list = pickle.load(driver_file)
 
     # initiate race window
     pygame.display.set_caption('Race!')
     screen = pygame.display.set_mode((width, height))
-    lines = False
-    display_checkpoints = False
 
     # create starting grid based on initial checkpoint
     spacing = 15
@@ -235,10 +214,10 @@ if Race:
 
     # load in drivers
     i = 0
-    for p in driver_list:
+    for car in driver_list:
         fov = random.uniform(0, 90)
-        env.addParticles(1, x=starting_grid[i][0], y=starting_grid[i][1], speed=0, size=5, control_rods=p.control_rods,
-                         bias=p.bias, fov=p.fov)
+        env.addParticles(1, x=starting_grid[i][0], y=starting_grid[i][1], speed=0, size=5, control_rods=car.control_rods,
+                         bias=car.bias, fov=car.fov)
         i += 1
     particle_list = env.particles
 
@@ -249,112 +228,24 @@ if Race:
 
     pygame.init()
 
-    # text preparation
-    basicfont = pygame.font.Font(None, 32)
-    text_string = 'LEADERBOARD'
-    header = basicfont.render(text_string, True, RED, WHITE)
-    headerRect = header.get_rect()
-    headerRect.center = (800 + 200, 40)
-
     # begin run
     running = True
     start_time = time.time()
     current_time = time.time()
-    while current_time - start_time < duration and running == True:
+    while current_time - start_time < duration and running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        # draw background
-        env.update()
-        screen.fill(env.colour)
-        screen.blit(track_image, track_rect)
+        # update cars + leaderboard
+        update_screen(env, screen, particle_list, track_image, track_rect)
 
-        # draw cars
-        for p in env.particles:
-            angle = math.pi - p.angle
-            fov = p.fov * math.pi / 180
-            if lines:
-                pygame.draw.line(screen, p.colour, (int(p.x), int(p.y)), (
-                    int(p.x) + p.distance_front * math.sin(angle), int(p.y) + p.distance_front * math.cos(angle)))
-                pygame.draw.line(screen, p.colour, (int(p.x), int(p.y)), (
-                    int(p.x) + p.distance_left * math.sin(angle + fov),
-                    int(p.y) + p.distance_left * math.cos(angle + fov)))
-                pygame.draw.line(screen, p.colour, (int(p.x), int(p.y)), (
-                    int(p.x) + p.distance_right * math.sin(angle - fov),
-                    int(p.y) + p.distance_right * math.cos(angle - fov)))
-            pygame.draw.circle(screen, p.colour, (int(p.x), int(p.y)), p.size, p.thickness)
-
-            sorted_list = sorted(particle_list, key=lambda particle: particle.score)[::-1]
-
-            # draw leaderboard
-
-            screen.blit(header, headerRect)
-
-            for i in range(0, 10):
-                p = sorted_list[i]
-
-                leader_x = 800 + 75
-                leader_y = 80 + i * 35
-
-                numberplate = basicfont.render(str(i + 1), True, BLACK, WHITE)
-                numberRect = numberplate.get_rect()
-                numberRect.center = (leader_x - 10, leader_y)
-                screen.blit(numberplate, numberRect)
-
-                pygame.draw.circle(screen, p.colour, (leader_x + 20, leader_y), p.size, p.thickness)
-
-                nameplate = basicfont.render(str(p.name), True, BLACK, WHITE)
-                nameRect = nameplate.get_rect()
-                nameRect.midleft = (leader_x + 45, leader_y)
-                screen.blit(nameplate, nameRect)
-
-                scoreplate = basicfont.render(str(round(p.score, 3)), True, BLACK, WHITE)
-                scoreRect = scoreplate.get_rect()
-                scoreRect.midleft = (leader_x + 100, leader_y)
-                screen.blit(scoreplate, scoreRect)
-
-                if p.w:
-                    wplate = basicfont.render('W', True, BLACK, WHITE)
-                else:
-                    wplate = basicfont.render('W', True, GREY, WHITE)
-                wRect = wplate.get_rect()
-                wRect.center = (leader_x + 190, leader_y)
-                screen.blit(wplate, wRect)
-
-                if p.a:
-                    aplate = basicfont.render('A', True, BLACK, WHITE)
-                else:
-                    aplate = basicfont.render('A', True, GREY, WHITE)
-                aRect = aplate.get_rect()
-                aRect.center = (leader_x + 212, leader_y)
-                screen.blit(aplate, aRect)
-
-                if p.s:
-                    splate = basicfont.render('S', True, BLACK, WHITE)
-
-                else:
-                    splate = basicfont.render('S', True, GREY, WHITE)
-                sRect = splate.get_rect()
-                sRect.center = (leader_x + 230, leader_y)
-                screen.blit(splate, sRect)
-
-                if p.d:
-                    dplate = basicfont.render('D', True, BLACK, WHITE)
-                else:
-                    dplate = basicfont.render('D', True, GREY, WHITE)
-                dRect = dplate.get_rect()
-                dRect.center = (leader_x + 250, leader_y)
-                screen.blit(dplate, dRect)
-
-            # draw checkpoints
-            if display_checkpoints:
-                for pos in checkpoints:
-                    pygame.draw.circle(screen, RED, pos, 5, 5)
-
-        pygame.display.flip()
         current_time = time.time()
         env.time_elapsed = int(round((current_time - start_time) * 100000)) / 100
 
-    # sort the cars, produce a final leaderboard
-    sorted_list = sorted(particle_list, key=lambda particle: particle.score)[::-1][:10]
+
+if __name__ == '__main__':
+    if Train:
+        train()
+    elif Race:
+        race()
